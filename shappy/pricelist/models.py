0dba8fa9ec5f2cc9081220a9dd2ecccb86cd78a4
Chris Waters <chris.waters@havas.com>
2021-09-02 19:27:27 +0100

    Created models for db and entered initial data

--- shappy/pricelist/models.py
+++ shappy/pricelist/models.py
@@ -1,10 +1,13 @@
 from django.db import models
 
 class Product(models.Model):
+    store = models.CharField(max_length=50, default='Aldi')
+    isle = models.IntegerField(null=True)
     code = models.CharField(max_length=10)
     description = models.CharField(max_length=200)
+    pack_size = models.IntegerField(default=1)
     price = models.CharField(max_length=200)
     def __str__(self):
-        template = '{0.code} {0.description} {0.price}'
+        template = '{0.store} {0.isle} {0.code} {0.description} {0.pack_size} {0.price}'
         return template.format(self)
 
