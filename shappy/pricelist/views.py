88942fe3039545f92c04eb1ac0a036e2962c1d7d
Chris Waters <cp-waters@hotmail.co.uk>
2021-09-02 21:06:40 +0100

    added details page

--- shappy/pricelist/views.py
+++ shappy/pricelist/views.py
@@ -1,13 +1,19 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.template import loader
+from django.http import HttpResponse, Http404
 from .models import Product
 
 
 def index(request):
     all_product_list = Product.objects.all()
-    output = ',<br>'.join([q.description for q in all_product_list])
-    return HttpResponse(output)
+    template = loader.get_template('pricelist/index.html')
+    context = { 'all_product_list' : all_product_list }
+    return HttpResponse(template.render(context, request))
 
 def detail(request, product_id):
-    return HttpResponse("%s" % product_id)
+    try:
+        product = Product.objects.get(pk=product_id)
+    except Product.DoesNotExist:
+        raise Http404("Product does not exist")
+    return render(request, 'pricelist/details.html', { 'product': product })
 
